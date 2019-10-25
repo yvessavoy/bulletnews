e64bfeb41693439eef6e4610bab8dbaadb236d39
Yves Savoy <ysavoy@gmail.com>
2019-10-25 18:11:37 +0200

    Add model for article

---
+++ web/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Article(models.Model):
+    ORIGINS = [
+        ('bbc', 'BBC'),
+    ]
+
+    title = models.CharField(max_length=200)
+    insert_tsd = models.DateTimeField('Insert Timestamp')
+    origin = models.CharField(max_length=50, choices=ORIGINS)
+    bp1 = models.TextField()
+    bp2 = models.TextField()
+    bp3 = models.TextField()
+    bp4 = models.TextField()
+    bp5 = models.TextField()
